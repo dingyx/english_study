fb824cb4d219fd760b5870528eab2616c8fa830c
dingyx <dyx1222@gmail.com>
2021-04-14 15:58:12 +0800

    modify ui

--- lib/WordStudyPage.dart
+++ lib/WordStudyPage.dart
@@ -37,10 +37,14 @@ class _WordStudyPageState extends State<WordStudyPage> {
   // 定时器
   Timer _timer;
 
+  // 单词timer 翻译延迟时间 单位/秒
+  int _wordTimer = 8;
+  int _tranDelay = 5;
+
   // 启动定时器
   void _startTimer() {
-    _timer = Timer.periodic(
-        Duration(seconds: int.parse(_wordEditController.text)), (timer) {
+    // 异常输入时 采用默认值
+    _timer = Timer.periodic(Duration(seconds: _wordTimer), (timer) {
       // 显示单词
       setState(() {
         ++_wordCount;
@@ -53,8 +57,7 @@ class _WordStudyPageState extends State<WordStudyPage> {
         tts.speak(_wordStr);
       }
       // 延迟显示音标、翻译
-      Future.delayed(Duration(seconds: int.parse(_tranEditController.text)),
-          () {
+      Future.delayed(Duration(seconds: _tranDelay), () {
         setState(() {
           _phoneticStr = WordData.getPhoneticSymbols();
           _wordTran = WordData.getTran();
@@ -67,12 +70,6 @@ class _WordStudyPageState extends State<WordStudyPage> {
   void _cancelTimer() {
     _timer?.cancel();
   }
-
-
-
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -94,81 +91,136 @@ class _WordStudyPageState extends State<WordStudyPage> {
                       fit: BoxFit.cover),
                 ),
                 child: Center(
-                    child: Column(mainAxisSize: MainAxisSize.max, children: <
-                        Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(left: 40, top: 40),
-                        child: Text(_wordCount.toString(),
-                            style: Styles.textNormal),
-                      ),
-                      InkWell(
-                          child: Padding(
-                            padding: EdgeInsets.only(right: 40, top: 40),
-                            child:
-                                Image.asset(_ttsImgPath, width: 40, height: 40),
+                    child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          InkWell(
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 40, top: 40),
+                                child: Image.asset(_ttsImgPath,
+                                    width: 40, height: 40),
+                              ),
+                              onTap: () {
+                                _isTTS = !_isTTS;
+                                setState(() {
+                                  if (_isTTS) {
+                                    _ttsImgPath =
+                                        "assets/images/sound_normal.png";
+                                  } else {
+                                    _ttsImgPath =
+                                        "assets/images/sound_mute.png";
+                                  }
+                                });
+                              }),
+                          Padding(
+                            padding: EdgeInsets.only(top: 40),
+                            child: Text(_wordCount.toString(),
+                                style: Styles.textNormal),
                           ),
-                          onTap: () {
-                            _isTTS = !_isTTS;
+                          InkWell(
+                              child: Padding(
+                                padding: EdgeInsets.only(right: 40, top: 40),
+                                child: Image.asset("assets/images/setting.png",
+                                    width: 34, height: 34),
+                              ),
+                              onTap: () {
+                                _showDialog(context);
+                              }),
+                        ],
+                      ),
+                      Expanded(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                            Text(_wordStr, style: Styles.textLarge),
+                            Text(_phoneticStr, style: Styles.textNormal),
+                            Text(_wordTran, style: Styles.textNormal),
+                          ])),
+                      SizedBox(height: 30),
+                      CupertinoSwitch(
+                          value: _isStudyStart,
+                          onChanged: (bool value) {
                             setState(() {
-                              if (_isTTS) {
-                                _ttsImgPath = "assets/images/sound_normal.png";
-                              } else {
-                                _ttsImgPath = "assets/images/sound_mute.png";
-                              }
+                              _isStudyStart = value;
                             });
-                          })
-                    ],
-                  ),
-                  Expanded(
-                      child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                        Text(_wordStr, style: Styles.textLarge),
-                        Text(_phoneticStr, style: Styles.textNormal),
-                        Text(_wordTran, style: Styles.textNormal),
-                      ])),
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: <
-                      Widget>[
-                    Text("Word Timer:", style: Styles.textSmall),
-                    Container(
-                        width: 60,
-                        child: CupertinoTextField(
-                            inputFormatters: [
-                              FilteringTextInputFormatter.allow(RegExp("[0-9]"))
-                            ],
-                            textAlign: TextAlign.center,
-                            controller: _wordEditController)),
-                    SizedBox(width: 30),
-                    Text("Translate Delay:", style: Styles.textSmall),
-                    Container(
-                        width: 60,
-                        child: CupertinoTextField(
-                            inputFormatters: [
-                              FilteringTextInputFormatter.allow(RegExp("[0-9]"))
-                            ],
-                            textAlign: TextAlign.center,
-                            controller: _tranEditController)),
-                  ]),
-                  SizedBox(height: 30),
-                  CupertinoSwitch(
-                      value: _isStudyStart,
-                      onChanged: (bool value) {
-                        setState(() {
-                          _isStudyStart = value;
-                        });
-                        // 点击按钮 启动关闭Timer
-                        if (value) {
-                          _startTimer();
-                        } else {
-                          _cancelTimer();
-                        }
-                        // 去掉输入框焦点
-                        FocusScope.of(context).requestFocus(FocusNode());
-                      }),
-                  SizedBox(height: 20)
-                ])))));
+                            // 点击按钮 启动关闭Timer
+                            if (value) {
+                              _startTimer();
+                            } else {
+                              _cancelTimer();
+                            }
+                            // 去掉输入框焦点
+                            FocusScope.of(context).requestFocus(FocusNode());
+                          }),
+                      SizedBox(height: 20)
+                    ])))));
+  }
+
+  // 点击弹出设置框
+  void _showDialog(widgetContext) {
+    showCupertinoDialog(
+      context: widgetContext,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text('Settings'),
+          content:
+              Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+                  Widget>[
+            SizedBox(height: 15),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Text("Word Timer:", style: Styles.textSmall),
+              SizedBox(width: 40),
+              Container(
+                  width: 60,
+                  child: CupertinoTextField(
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp("[0-9]"))
+                      ],
+                      textAlign: TextAlign.center,
+                      controller: _wordEditController)),
+            ]),
+            SizedBox(height: 10),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Text("Translate Delay:", style: Styles.textSmall),
+              SizedBox(width: 10),
+              Container(
+                  width: 60,
+                  child: CupertinoTextField(
+                      inputFormatters: [
+                        FilteringTextInputFormatter.allow(RegExp("[0-9]"))
+                      ],
+                      textAlign: TextAlign.center,
+                      controller: _tranEditController)),
+            ]),
+          ]),
+          actions: [
+            CupertinoDialogAction(
+              child: Text('Confirm'),
+              onPressed: () {
+                try {
+                  _wordTimer = int.parse(_wordEditController.text);
+                  _tranDelay = int.parse(_tranEditController.text);
+                  if (_tranDelay > _wordTimer) {
+                    _wordTimer = 8;
+                    _tranDelay = 5;
+                  }
+                } catch (ignore) {}
+                Navigator.of(context).pop();
+              },
+            ),
+            CupertinoDialogAction(
+              child: Text('Cancel'),
+              isDestructiveAction: true,
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
